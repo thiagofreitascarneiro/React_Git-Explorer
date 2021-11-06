329b7ee30553272eaa530fae481d4f9de4099113
thiago <thiagofreitascarneiro@hotmail.com>
2021-11-06 14:11:53 -0300

    add typescript in the project

---
+++ src/components/Repositoryitem.tsx
@@ -0,0 +1,21 @@
+interface RepositoryItemProps {
+    repository: {
+        name: String;
+        description: string;
+        html_url: string;
+    }
+}
+
+
+export function RepositoryItem(props) {
+    return (
+        <li>
+            <strong>{props.repository.name}</strong>
+            <p>{props.repository.description}</p>
+
+            <a href={props.repository.html_url}>
+                acessar repositório
+            </a>
+        </li>
+        );
+}
